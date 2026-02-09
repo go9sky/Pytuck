6a5ec7125667a1ae3f51efff1d367e028f74764d
天霄 <go9sky@outlook.com>
2026-02-09 23:54:51 +0800

    chore(release): 更新版本号到 0.7.0

--- pytuck/__init__.py
+++ pytuck/__init__.py
@@ -93,7 +93,7 @@ from .common.exceptions import (
 )
 from .common.options import SyncOptions, SyncResult
 
-__version__ = '0.6.3'
+__version__ = '0.7.0'
 __all__ = [
     # ==================== 推荐 API ====================
 
